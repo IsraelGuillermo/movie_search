6c286a05bd21aaceb90565615384d3c480067eb6
Israel Guillermo <israelguillermo22@gmail.com>
2020-12-16 22:20:07 -0600

    updated api call, now getting information for movie searched.

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const movie = document.querySelector("#movie");
 search.addEventListener("click", () => {
   let key = "34bbd0fa";
   let selectedMovie = movie.value.trim();
-  let query = "http://www.omdbapi.com/?apikey=" + key + "&s=" + selectedMovie;
+  let query =
+    "http://www.omdbapi.com/?apikey=" + key + "&plot=short&s=" + selectedMovie;
   console.log(query);
 
   $.ajax({
     url: query,
     method: "GET",
   }).then(function (results) {
-    console.log(results);
+    // console.log(results.Search[0].Title);
+    for (let i = 0; i < 10; i++) {
+      let div = $("<div>");
+      $("<h1>").text(results.Search[i].Title).appendTo(div);
+      // $("<p>").text(results.Search[i].appendTo(div);
+      div.appendTo("#movies");
+    }
   });
 });
